2673108cb99c15514ea7aac59daeab40dfc4378b
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-07-11 19:00:41 +0000

    commit

--- src/GLOBAL/components/cards/testimonialCard.js
+++ src/GLOBAL/components/cards/testimonialCard.js
@@ -1,45 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { tsCardImg } from "../../../utils/assets";
+
 const TestimonialCard = ({ testimonial, index, currentIndex }) => {
-  // const [activeIndex, setActiveIndex] = useState(0);
-  // console.log(activeIndex)
  
   return (
     <div
-                  key={testimonial.id}
-                  className={`ts-card ${
-                    index === currentIndex ? "" : "shadow-card"
-                  }`}
-                >
-                  <div
-                    className={`${
-                      index === currentIndex ? "group-item" : "group-child"
-                    }`}
-                  ></div>
-                  <img
-                    className="ts-card-img"
-                    alt={testimonial.author}
-                    src={tsCardImg}
-                  />
-                  <div className="testimonial">{index === currentIndex ? testimonial.text : ''}</div>
-                  <div
-                    className={`ts-card-author ${
-                      index === currentIndex ? "active" : ""
-                    }`}
-                  >
-                    {testimonial.author},
-                  </div>
-                  <div
-                    className={`ts-card-position ${
-                      index === currentIndex ? "active" : ""
-                    }`}
-                  >
-                    {testimonial.position}
-                  </div>
-                </div>
-  )
-
-  
+      key={testimonial.id}
+      className={`ts-card ${index === currentIndex ? "" : "shadow-card"}`}
+    >
+      <div className={`${index === currentIndex ? "group-item" : "group-child"}`}></div>
+      <img
+        className={`ts-card-img ${index === currentIndex ? "visible" : "hidden"}`}
+        alt={testimonial.author}
+        src={tsCardImg}
+      />
+      <div className={`testimonial ${index === currentIndex ? "visible" : "hidden"}`}>{testimonial.text}</div>
+      <div className={`ts-card-author ${index === currentIndex ? "active" : ""}`}>
+        {testimonial.author},
+      </div>
+      <div className={`ts-card-position ${index === currentIndex ? "active" : ""}`}>
+        {testimonial.position}
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialCard;
+
