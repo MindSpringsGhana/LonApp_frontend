b77274604b224b8370e1a3e02fe4d6ff33f41c28
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-07-11 16:32:49 +0000

    commit

--- src/GLOBAL/components/monthlyYearlyToggler.js
+++ src/GLOBAL/components/monthlyYearlyToggler.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 import '../../GLOBAL/styles/monthly-yearly-toggler.scss'
 const   MonthlyYearlyToggler = ({  }) => {
-
- 
+  const [yearly, setYearly] = useState(false)
+  
+ console.log(yearly)
   return (
-    <div class="full-toggle-parent">
-    <div class="full-toggle">
-      <div class="monthly">Monthly</div>
-      <div class="rectangle-parent">
-        <div class="rectangle1"></div>
-        <div class="oval"></div>
+    <div className="full-toggle-parent">
+    <div className="full-toggle">
+      <div className="monthly">Monthly</div>
+      <div className="rectangle-parent" onClick={() => setYearly(!yearly)}>
+        <div className="rectangle1"></div>
+        <div className={`oval ${yearly && 'yearly'}`}></div>
       </div>
-      <div class="yearly">Yearly</div>
-      <div class="tagtag-02">
-        <div class="tag">
-          <div class="rectangle"></div>
-          <b class="save-25">Save 10%</b>
+      <div className="yearly">Yearly</div>
+      <div className="tagtag-02">
+        <div className="tag">
+          <div className="rectangle"></div>
+          <b className="save-25">Save 10%</b>
         </div>
       </div>
     </div>
