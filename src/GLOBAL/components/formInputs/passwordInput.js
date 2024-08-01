bbbdf7027eb0d0418a61a7e6799c4266edb9115e
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-08-01 16:20:09 +0000

    commit

--- src/GLOBAL/components/formInputs/passwordInput.js
+++ src/GLOBAL/components/formInputs/passwordInput.js
@@ -1,19 +1,29 @@
 // components/PasswordInput.js
 import React, { useState } from "react";
-
-const PasswordInput = ({ label, name, value, onChange, placeholder, error, inputStarted }) => {
+import '../../styles/password-input.scss'
+const PasswordInput = ({
+  label,
+  name,
+  value,
+  onChange,
+  placeholder,
+  error,
+  inputStarted,
+}) => {
   const [showPassword, setShowPassword] = useState(false);
-
+  const location = window.location.pathname;
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
-  console.log(inputStarted)
+  console.log(inputStarted);
   return (
     <div className="form-group">
       <label className="field-label">{label}</label>
       <div className="password-wrapper">
         <input
-          className={`form-control password-form-control ${error && "error"} ${inputStarted && "entry-background"}`}
+          className={`form-control password-form-control ${error && "error"} ${
+            inputStarted && "entry-background"
+          }`}
           name={name}
           type={showPassword ? "text" : "password"}
           placeholder={placeholder}
@@ -25,6 +35,19 @@ const PasswordInput = ({ label, name, value, onChange, placeholder, error, input
           onClick={togglePasswordVisibility}
         ></div>
       </div>
+
+      {location === "/signup" && (
+        <div className="strength-bar-wrapper">
+        <div className="password-strength-bar">
+          <div className="password-progress-bar">
+            <div className="background"></div>
+            <div className="password-progress"></div>
+          </div>
+          
+        </div>
+        <b className="password-percentage">Strong Password</b>
+        </div>
+      )}
     </div>
   );
 };
